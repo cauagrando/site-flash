49c9eabbefcb2502ced23e4b027663a2639c81e1
cauagrando <159459644+cauagrando@users.noreply.github.com>
2024-12-02 11:46:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Espaço',
+    '1. Como se sabe a idade do universo? ',
+    'Existem várias maneiras de calcular a idade do universo, incluindo a constante de Hubble, características das estrelas e isótopos radioativos em meteoritos para estimar a sua data de nascimento.'
+)
+
+criaCartao(
+    'Espaço',
+    'As estrelas podem se apagar um dia?',
+    'Estrelas perdem energia luminosa ao longo de bilhões de anos. Pode ocorrer de três formas: anãs brancas, explosões ou buracos negros, dependendo da massa estelar.'
+)
+
+criaCartao(
+    'Espaço',
+    'Um buraco negro pode engolir outro?',
+    
+    'Teoricamente, não existem limites para a massa que os buracos negros podem engolir. Portanto, eles poderiam absorver matéria indefinidamente. Um buraco negro não pode engolir outro, mas eles podem se unir, formando buracos negros ainda maiores.'
+)
+criaCartao(
+    'Espaço',
+    'Há regras para a colonização do espaço?',
+    'Em 1967, Tratado do Espaço da ONU proíbe apropriação de corpos celestes. Tentativa de regular exploração em 1979 com Acordo da Lua fracassa. Espaço é de uso comum.'
+)
+
+criaCartao(
+    'Espaço',
+    'Por que existem estrelas de diferentes cores?',
+    'As cores das estrelas mudam pela composição química e temperatura: 3 000 graus (vermelho) e 30 000 graus (azul).'
+)
+
+criaCartao(
+    'Espaço',
+    ' Qual a maior estrela conhecida?',
+    'A estrela Pistola é a maior em massa e brilho, enquanto a gigante VV Cephei é a maior em tamanho, 4000 vezes maior que o Sol. Mercúrio, Vênus, Terra, Marte e Júpiter seriam engolidos por ela.'
+)
+
+criaCartao(
+    'Espaço',
+    'Por que um Boeing não consegue entrar em órbita?',
+    'Para entrar em órbita, um objeto precisa voar acima da "velocidade de escape" da Terra, muito além da velocidade do som.'
+)
